7d401fe39322b069eb5adee45e5324c6ffffbce7
Devin Miller <devin@nextdrop.org>
2015-08-27 22:43:55 +0530

    Initial commit of the repository!

---
+++ README.rst
@@ -0,0 +1,25 @@
+===========================
+README for hello_webcrawler
+===========================
+
+This is the brief documentation and starter kit for the hello web-crawler task.  It uses flask, a lightweight Python web framework.  The project contains two simple views and two simple unit tests.
+
+
+Running the project
+===================
+
+Install Flask::
+
+    pip install flask
+
+Run the server::
+
+    python hello.py
+
+Access the URL to see if the server is up::
+
+    http://127.0.0.1:5000/
+
+To run the unit tests::
+
+    python hello_tests.py
---
+++ hello_tests.py
@@ -0,0 +1,27 @@
+import os
+import unittest
+import tempfile
+import hello
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, hello.app.config['DATABASE'] = tempfile.mkstemp()
+        self.app = hello.app.test_client()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(hello.app.config['DATABASE'])
+
+    def test_welcome_response(self):
+        rv = self.app.get('/')
+        assert 'Welcome!' in rv.data
+
+    def test_hello_world_response(self):
+        rv = self.app.get('/hello')
+        assert 'Hello World!' in rv.data
+
+
+if __name__ == '__main__':
+    unittest.main()
