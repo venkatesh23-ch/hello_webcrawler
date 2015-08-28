8b7f84c785fbe131823b8cb2d0144743cb50a4ef
venkatesh <venkatesh23.ch@gmail.com>
2015-08-28 18:39:53 +0530

    modified:   README.rst 	modified:   hello.py 	modified:   hello_tests.py 	templates/

--- hello.py
+++ hello.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def welcome():
-    return 'Welcome!'
-
+    # return 'Welcome!'
+    return render_template('index.html')
 
 @app.route('/hello')
 def hello_world():
     return 'Hello World!'
 
+@app.route("/crawl", methods=['POST'])
+def crawl():
+    website = request.form['websiteUrl']
+    return render_template('crawlResults.html', name=website)
+    # return "Crawling -------- "+website
+
 if __name__ == '__main__':
+    app.debug = True
     app.run()
